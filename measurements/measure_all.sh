551815f2b8fd6ee96a0c6739dc30848f40b91f20
Teyras <Teyras@gmail.com>
2018-05-07 00:22:51 +0200

    Drop measurements for some numbers of workers

--- measurements/measure_all.sh
+++ measurements/measure_all.sh
@@ -5,7 +5,7 @@ cat workloads.txt | while read workload size iters; do
 	./measure_workload.sh "isolate single" runners/run_isolate.sh $workload $size $iters
 done
 
-for workers in 2 4 8 10 16 20 32 40; do
+for workers in 2 10 20 40; do
 	cat workloads.txt | while read workload size iters; do
 		./measure_parallel_homogenous.sh $workers "bare parallel-homogenous" runners/run_baremetal.sh $workload $size $iters
 		./measure_parallel_homogenous.sh $workers "isolate parallel-homogenous" runners/run_baremetal.sh $workload $size $iters
