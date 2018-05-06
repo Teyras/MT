2db2984bd5a5cdfc91063df4dfa2200b74c02bbe
Teyras <Teyras@gmail.com>
2018-05-07 01:02:32 +0200

    Implement measuring with taskset

--- measurements/measure_all.sh
+++ measurements/measure_all.sh
@@ -9,5 +9,8 @@ for workers in 2 10 20 40; do
 	cat workloads.txt | while read workload size iters; do
 		./measure_parallel_homogenous.sh $workers "bare parallel-homogenous" runners/run_baremetal.sh $workload $size $iters
 		./measure_parallel_homogenous.sh $workers "isolate parallel-homogenous" runners/run_baremetal.sh $workload $size $iters
+
+		./measure_parallel_homogenous.sh --taskset $workers "bare parallel-homogenous" runners/run_baremetal.sh $workload $size $iters
+		./measure_parallel_homogenous.sh --taskset $workers "isolate parallel-homogenous" runners/run_baremetal.sh $workload $size $iters
 	done
 done
--- measurements/measure_parallel_homogenous.sh
+++ measurements/measure_parallel_homogenous.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+if [ "$1" == "--taskset" ]; then
+	taskset=1
+	shift
+fi
+
 workers=$1
 shift
 
 label=$1
 shift
 
-seq 1 $workers | parallel -j$workers "./measure_workload.sh \"$label,worker-{}/$workers\" $@"
+cmd="./measure_workload.sh \"$label,$workers,cpu-{}\" $@"
+
+if [ -n "$taskset" ]; then
+	cmd="taskset -c {} $cmd"
+fi
+
+top_worker=$(($(nproc --all) - 1))
+step=$((2 * $(nproc --all) / $workers))
+
+(seq 0 $step $top_worker && seq 1 $step $top_worker) | parallel -j$workers "$cmd"
