372d2bb19de42e55b7921bf507f165c6b36d984d
Jan Buchar <Teyras@gmail.com>
2018-12-11 13:38:33 +0100

    A working prototype of broker simulator

--- broker_lb/simulator.cpp
+++ broker_lb/simulator.cpp
@@ -9,9 +9,13 @@
 #include "broker/src/queuing/multi_queue_manager.h"
 
 
-void populate_registry(std::shared_ptr<worker_registry> registry)
+void load_workers(broker_handler &handler)
 {
-
+    handler.on_request(message_container(
+            broker_connect::KEY_WORKERS,
+            "worker_1",
+            {"init", "group_1", "env=c", ""}
+        ), [] (const message_container &) {});
 }
 
 std::shared_ptr<queue_manager_interface> create_queue_manager(const std::string &type)
@@ -26,9 +30,8 @@ std::shared_ptr<queue_manager_interface> create_queue_manager(const std::string 
 struct simulation_job {
     std::string job_id;
     std::vector<std::string> data;
-    boost::posix_time::milliseconds processing_time; // How long the job takes
     boost::posix_time::milliseconds arrival_time;
-    boost::posix_time::milliseconds enqueueing_time;
+    boost::posix_time::milliseconds processing_time; // How long the job takes
     boost::posix_time::milliseconds processing_started_time;
 };
 
@@ -41,9 +44,39 @@ struct job_compare {
     }
 };
 
+using job_queue = std::set<simulation_job, job_compare>;
+using job_data = std::map<std::string, simulation_job>;
+
+void load_jobs(job_data &jobs)
+{
+    jobs.emplace("job_1", simulation_job{
+        .job_id="job_1",
+        .data={"eval", "job_1", "env=c", ""},
+        .arrival_time=boost::posix_time::milliseconds(100),
+        .processing_time=boost::posix_time::milliseconds(1500),
+        .processing_started_time=boost::posix_time::milliseconds(0)
+    });
+
+    jobs.emplace("job_2", simulation_job{
+        .job_id="job_2",
+        .data={"eval", "job_2", "env=c", ""},
+        .arrival_time=boost::posix_time::milliseconds(200),
+        .processing_time=boost::posix_time::milliseconds(1300),
+        .processing_started_time=boost::posix_time::milliseconds(0)
+    });
+
+    jobs.emplace("job_3", simulation_job{
+        .job_id="job_3",
+        .data={"eval", "job_3", "env=c", ""},
+        .arrival_time=boost::posix_time::milliseconds(3000),
+        .processing_time=boost::posix_time::milliseconds(500),
+        .processing_started_time=boost::posix_time::milliseconds(0)
+    });
+}
+
 class responder {
 private:
-    worker_status_map &worker_status_;
+    worker_status_map worker_status_;
     broker_handler &handler_;
     bool is_terminating_ = false;
     boost::asio::io_service &io_;
@@ -53,26 +86,33 @@ private:
     boost::asio::deadline_timer ping_timer_;
     boost::asio::deadline_timer reactor_periodic_timer_;
 
-    std::set<simulation_job, job_compare> incoming_jobs_; // Used as a queue for incoming jobs
-    std::map<std::string, simulation_job> job_data_;
+    job_queue incoming_jobs_;
+    job_data &job_data_;
 
     boost::posix_time::ptime start_time_;
 
-    boost::posix_time::milliseconds time_since_start() const
-    {
-        return boost::posix_time::milliseconds(
-            (boost::posix_time::microsec_clock::universal_time() - start_time_).total_milliseconds());
-    }
-
 public:
-    responder(worker_status_map &worker_status, broker_handler &handler, boost::asio::io_service &io):
-        worker_status_(worker_status),
+    responder(broker_handler &handler, boost::asio::io_service &io, job_data &jobs, std::shared_ptr<worker_registry> registry):
         handler_(handler),
         io_(io),
         job_timer_(io_),
         ping_timer_(io_),
-        reactor_periodic_timer_(io_)
+        reactor_periodic_timer_(io_),
+        job_data_(jobs)
     {
+        for (auto &it: jobs) {
+            incoming_jobs_.insert(it.second);
+        }
+
+        for (auto &worker: registry->get_workers()) {
+            worker_status_[worker->identity] = true;
+        }
+    }
+
+    boost::posix_time::milliseconds time_since_start() const
+    {
+        return boost::posix_time::milliseconds(
+                (boost::posix_time::microsec_clock::universal_time() - start_time_).total_milliseconds());
     }
 
     void start()
@@ -119,9 +159,14 @@ public:
 
     void send_job_done(const std::string &job_id, const std::string &worker_identity)
     {
-        handler_.on_request(message_container(broker_connect::KEY_WORKERS, worker_identity, {"done"}), [this] (const auto &message) {
-            respond(message);
-        });
+        handler_.on_request(message_container(
+                broker_connect::KEY_WORKERS,
+                worker_identity,
+                {"done", job_id, "OK"}
+            ), [this] (const auto &message) {
+                respond(message);
+            }
+        );
     }
 
     void send_jobs()
@@ -187,7 +232,7 @@ public:
                 message_container(
                         broker_connect::KEY_TIMER,
                         "",
-                        {std::to_string(interval.ticks())}
+                        {std::to_string(interval.total_milliseconds())}
                 ),
                 [this] (const auto &msg) {
                     respond(msg);
@@ -210,22 +255,41 @@ int main(int argc, char **argv)
     // For maximum liveness and ping interval, we will use the default values.
     auto config = std::make_shared<const broker_config>();
 
-    // Fill the registry according to specification from the command line
-    auto registry = std::make_shared<worker_registry>();
-    populate_registry(registry);
-
     // Create one of the predefined queue managers
     auto queue = create_queue_manager("multi");
 
-    broker_handler handler(config, registry, queue, std::shared_ptr<spdlog::logger>());
+    auto registry = std::make_shared<worker_registry>();
+
+    // Create a logger that outputs to stderr
+    auto sink = std::make_shared<spdlog::sinks::stderr_sink_mt>();
+    auto logger = spdlog::create("broker", sink);
+    logger->set_level(spdlog::level::debug);
+
+    broker_handler handler(config, registry, queue, logger);
+
+    // Fill the registry and queue according to specification from the command line
+    load_workers(handler);
+
+    // Load jobs to be simulated
+    job_data jobs;
+    load_jobs(jobs);
 
     boost::asio::io_service io;
 
-    worker_status_map worker_status;
-
-    responder res{worker_status, handler, io};
+    responder res{handler, io, jobs, registry};
     res.start();
     
     io.run();
+
+    std::cerr << "Done in " << std::to_string(res.time_since_start().total_milliseconds()) << "ms" << std::endl;
+
+    for (auto &it: jobs) {
+        std::cout
+            << it.second.job_id << " "
+            << it.second.arrival_time.total_milliseconds() << " "
+            << it.second.processing_time.total_milliseconds() << " "
+            << it.second.processing_started_time.total_milliseconds() << std::endl;
+    }
+
     return 0;
 }
