2075ce774c3a537d287ac262336f89973301cec8
Jan Buchar <Teyras@gmail.com>
2020-02-15 13:43:38 +0100

    Use a tree container for job queue instead of heap

--- broker_lb/queue_managers/single_queue_manager.h
+++ broker_lb/queue_managers/single_queue_manager.h
@@ -13,7 +13,7 @@ struct request_entry {
 struct fcfs_job_comparator {
     bool operator()(const request_entry &a, const request_entry &b) const
     {
-        return a.arrived_at > b.arrived_at;
+        return a.arrived_at < b.arrived_at;
     }
 };
 
@@ -21,12 +21,14 @@ template <typename JobComparator>
 class single_queue_manager : public queue_manager_interface
 {
 private:
-    std::vector<request_entry> jobs_;
-    std::map<worker_ptr, request_ptr> worker_jobs_;
     std::unique_ptr<JobComparator> comparator_;
+    std::multiset<request_entry, JobComparator> jobs_;
+    std::map<worker_ptr, request_ptr> worker_jobs_;
 
 public:
-    explicit single_queue_manager(std::unique_ptr<JobComparator> comparator): comparator_(std::move(comparator))
+    explicit single_queue_manager(std::unique_ptr<JobComparator> comparator):
+        comparator_(std::move(comparator)),
+        jobs_(*comparator_)
     {
     }
 
@@ -45,21 +47,18 @@ public:
 
     request_ptr assign_request(worker_ptr worker) override
     {
-        if (jobs_.empty()) {
-            worker_jobs_[worker] = nullptr;
-            return nullptr;
+        for (auto it = jobs_.cbegin(); it != jobs_.cend(); ++it) {
+            if (!worker->check_headers(it->request->headers)) {
+                continue;
+            }
+
+            worker_jobs_[worker] = it->request;
+            jobs_.erase(it);
+
+            return it->request;
         }
 
-        // Fix the heap in case the job order changed in time
-        std::make_heap(std::begin(jobs_), std::end(jobs_), *comparator_);
-
-        // Extract the maximum from the heap
-        std::pop_heap(std::begin(jobs_), std::end(jobs_), *comparator_);
-        auto job = jobs_.back();
-        jobs_.pop_back();
-
-        worker_jobs_[worker] = job.request;
-        return job.request;
+        return nullptr;
     }
 
     std::shared_ptr<std::vector<request_ptr>> worker_terminated(worker_ptr worker) override
@@ -99,12 +98,10 @@ public:
         }
 
         // Enqueue the job
-        jobs_.push_back(request_entry{
+        jobs_.insert(request_entry{
             .request = request,
             .arrived_at = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now())
         });
-
-        std::push_heap(std::begin(jobs_), std::end(jobs_), *comparator_);
 
         return enqueue_result{
             .assigned_to = nullptr,
