7c7990d1927db2b0d1365b8538e0fe10cb6385aa
Jan Buchar <Teyras@gmail.com>
2020-03-12 15:07:11 +0100

    Basic measurements and evalation

--- broker_lb/measure.sh
+++ broker_lb/measure.sh
@@ -8,38 +8,42 @@ LOGS_DIR=$ROOT_DIR/logs
 
 results_file=$ROOT_DIR/results-lb.$(date '+%Y-%m-%d_%H:%M:%S').csv
 mkdir -p $LOGS_DIR
+rm $LOGS_DIR/*
 
-pushd $WORKLOADS_DIR/generators/
-python simple+para.py --count 1000 --avg-delay 30 --simple-duration 300 100 --para-rate 25 --para-duration 600 200 \
-                      > $WORKLOADS_DIR/simple+para_small.csv
-python simple+para.py --count 4000 --avg-delay 30 --simple-duration 300 100 --para-rate 25 --para-duration 600 200 \
-                      > $WORKLOADS_DIR/simple+para_large.csv
+generate_inputs() {
+    pushd $WORKLOADS_DIR/generators/ > /dev/null
 
-python two_phase.py --count 1000 --avg-delay 30 --phase-threshold 600 --second-rate 80 --first-duration 300 100 \
-                    --second-duration 500 150 > $WORKLOADS_DIR/two_phase_small.csv
-python two_phase.py --count 4000 --avg-delay 30 --phase-threshold 2400 --second-rate 80 --first-duration 300 100 \
-                    --second-duration 500 150 > $WORKLOADS_DIR/two_phase_large.csv
-popd
+    python simple+para.py --count 1000 --avg-delay 30 --simple-duration 300 100 --para-rate 25 --para-duration 600 200 \
+                          > $WORKLOADS_DIR/simple+para_small.csv
+    python simple+para.py --count 4000 --avg-delay 30 --simple-duration 300 100 --para-rate 25 --para-duration 600 200 \
+                          > $WORKLOADS_DIR/simple+para_large.csv
 
-run_simulator() {
-    setup_arg=$1
-    workload_arg=$2
+    python two_phase.py --count 1000 --avg-delay 70 --phase-threshold 600 --second-rate 80 --first-duration 300 100 \
+                        --second-duration 500 150 > $WORKLOADS_DIR/two_phase_small.csv
 
-    for queue_manager in multi single_fcfs; do
-        $SIMULATOR $queue_manager $SETUPS_DIR/$setup_arg.yml $WORKLOADS_DIR/$workload_arg.csv \
-            2> $LOGS_DIR/$queue_manager,$setup_arg,$workload_arg.log |
-            sed "s/^/$queue_manager,$setup_arg,$workload_arg,/"
-    done
+    popd > /dev/null
 }
 
-for variant in small large; do
-    workload=simple+para-$variant
-    run_simulator simple+para-small $workload >> $results_file
-    run_simulator simple+para-large $workload >> $results_file
+arg_sets=$(mktemp)
+queue_managers="multi single_fcfs single_lf single_spt_oracle single_edf_oracle oagm_oracle multi_ll_queue_size multi_ll_oracle multi_rand2_queue_size multi_rand2_oracle"
+for queue_manager in $queue_managers; do
+    workload=simple+para_small
+    echo $queue_manager simple+para-small $workload >> $arg_sets
+    echo $queue_manager simple+para-large $workload >> $arg_sets
+
+    workload=simple+para_large
+    echo $queue_manager simple+para-large $workload >> $arg_sets
+
+    workload=two_phase_small
+    echo $queue_manager two-types-small $workload >> $arg_sets
+    echo $queue_manager two-types-large $workload >> $arg_sets
 done
 
-for variant in small large; do
-    workload=two-types-$variant
-    run_simulator two-types-small $workload >> $results_file
-    run_simulator two-types-large $workload >> $results_file
-done
+generate_inputs
+
+cat $arg_sets | parallel --colsep=" " --tagstring={1},{2},{3}, --joblog $LOGS_DIR/jobs.tsv \
+    $SIMULATOR {1} $SETUPS_DIR/{2}.yml $WORKLOADS_DIR/{3}.csv \
+    "2>" $LOGS_DIR/{1},{2},{3}.log > $results_file
+
+rm $arg_sets
+
--- broker_lb/plots/plot-lb-efficiency.r
+++ broker_lb/plots/plot-lb-efficiency.r
@@ -1,22 +1,86 @@
 library("tikzDevice")
 library("ggplot2")
 library("tidyr")
+library("dplyr")
 
 source("../../measurements/plots/helpers.r")
 
 values <- load.lb.results(filename.from.args())
 values$wait.time <- values$processing.start - values$arrival
 values$relative.wait.time <- values$wait.time / values$processing.time
+values$queue.manager <- tex.safe(values$queue.manager)
+values$workload <- tex.safe(values$workload)
+values$processing.end <- values$processing.start + values$processing.time
+values$lateness.class <- cut(values$relative.wait.time, breaks=c(0, 0.1, 1, 3, Inf), labels=c("on.time", "delayed", "late", "extremely.late"), right=F)
 
-values <- gather(values, key="metric", value="value", wait.time, relative.wait.time)
+plot.efficiency <- function() {
+	mkdir("lb-efficiency")
+	narrow.values <- gather(values, key="metric", value="value", wait.time, relative.wait.time)
 
-for (setup in unique(values$setup)) {
-	tikz(file=paste("lb-efficiency-", setup, ".tex", sep=""))
+	for (setup in unique(narrow.values$setup)) {
+		file <- paste("lb-efficiency/", tex.unsafe(setup), sep="")
+		tikz(file=paste(file, ".tex", sep=""))
 
-	plot <- ggplot(values[values$setup == setup, ], aes(x=queue.manager, y=value)) +
-		geom_boxplot() +
-		facet_grid(rows=vars(metric), cols=vars(workload), scales="free")
+		plot <- ggplot(narrow.values[narrow.values$setup == setup, ], aes(x=queue.manager, y=value)) +
+			geom_violin() +
+			facet_grid(rows=vars(metric), cols=vars(workload), scales="free") +
+			theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+		print(plot)
+		ggsave(file=paste(file, ".png", sep=""), plot)
+		dev.off()
+	}
+}
+
+plot.makespan <- function() {
+	makespans <- aggregate(values$processing.end, list(queue.manager=values$queue.manager, setup=values$setup, workload=values$workload), max)
+
+	tikz(file="makespans.tex", width=15, height=20)
+	plot <- ggplot(makespans, aes(x=queue.manager, y=x)) +
+		geom_col() +
+		facet_wrap(~ setup + workload, scales="free", ncol=2) +
+		theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 	print(plot)
 	dev.off()
 }
+
+plot.wait.time.trends <- function() {
+	dir <- "wait-time-trends"
+	mkdir(dir)
+
+	res <- group_walk(group_by(values, setup, workload), function(data, group) {
+		# Lateness graph
+		file <- tex.unsafe(paste(dir, "/lateness,", group$setup, ",", group$workload, sep=""))
+		tikz(file=paste(file, ".tex", sep=""))
+
+		arrival.breaks <- c(seq(0, max(data$arrival), max(data$arrival) / 20), Inf)
+		arrival.breaks <- quantile(data$arrival, seq(0, 1, .05))
+		data$arrival.discrete = cut(data$arrival, breaks=arrival.breaks, labels=1:20, include.lowest=T)
+
+		plot <- ggplot(data, aes(arrival.discrete)) +
+			geom_bar(aes(fill=lateness.class), position=position_fill(reverse=T)) +
+			scale_fill_manual(values=c("on.time"="darkgreen", "delayed"="green", "late"="pink", "extremely.late"="red")) +
+			facet_wrap(vars(data$queue.manager), ncol=2)
+		print(plot)
+		ggsave(file=paste(file, ".png", sep=""), plot)
+		dev.off()
+
+		# Relative wait time histogram
+		file <- tex.unsafe(paste(dir, "/rel_wait_time,", group$setup, ",", group$workload, sep=""))
+		tikz(file=paste(file, ".tex", sep=""))
+
+		plot <- ggplot(data, aes(relative.wait.time)) +
+			geom_histogram() +
+			scale_y_log10() +
+			facet_wrap(vars(data$queue.manager), ncol=2)
+		print(plot)
+		ggsave(file=paste(file, ".png", sep=""), plot)
+		dev.off()
+	})
+}
+
+
+plot.efficiency()
+plot.makespan()
+plot.wait.time.trends()
