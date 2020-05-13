5adcb389ca4167acca8a1d9969c4110069b3d120
Jan Buchar <Teyras@gmail.com>
2020-05-13 22:48:45 +0200

    Images and scripts

---
+++ containers/plots/plot-mount-vs-unpack.r
@@ -0,0 +1,21 @@
+source("../../measurements/plots/helpers.r")
+
+library("ggplot2")
+library("tikzDevice")
+library("scales")
+
+values <- read.csv(filename.from.args())
+names(values) <- c("image", "implementation", "time")
+values$time <- values$time / (10 * 1000)
+
+make.plot <- function() {
+	tikz("mount-vs-unpack.tex", width=5.5, height=4)
+	plot <- ggplot(values, aes(x=image, y=time, color=implementation)) +
+		geom_boxplot(position="dodge") +
+		labs(x="Image", y="Time per operation [s]", color="Method")
+
+	print(plot)
+	ggsave("mount-vs-unpack.png")
+}
+
+make.plot()
